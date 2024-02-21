539b560f9159a786a9064df4d2930c34ca35ec14
Mosiah Valdez <val20004@byui.edu>
2024-02-21 14:18:47 -0700

    Touching up assignments

--- lesson7/scripts/bom.js
+++ lesson7/scripts/bom.js
@@ -1,30 +1,21 @@
-const input = document.querySelector('#favchap')
-const button = document.querySelector('button')
+const input = document.querySelector('#favchap');
+const addButton = document.querySelector('button');
 const deleteAllButton = document.querySelector('#deleteAllButton');
-const list = document.querySelector('#list')
-let chaptersArray = getChapterList() ||[];
+const list = document.querySelector('#list');
 
-button.addEventListener('click', function() {
-    if (input.value != '') {
-        
-        const li = document.createElement('li');
-    
-        const deleteButton = document.createElement('button');
-        
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
-    
-        li.append(deleteButton);
-        list.append(li);
-    
-        deleteButton.addEventListener('click', function () {
-            list.removeChild(li);
-            input.focus();
-        });
-        
-        input.focus();
-        
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+addButton.addEventListener('click', function() {
+    if (input.value !== '') {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
         input.value = '';
+        input.focus();
     }
 });
 
@@ -32,13 +23,36 @@ deleteAllButton.addEventListener('click', function() {
     while (list.firstChild) {
         list.removeChild(list.firstChild);
     }
+    chaptersArray = [];
+    setChapterList();
 });
 
 function getChapterList() {
-
-    return [];
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
 }
 
-chaptersArray.forEach(chapter => {
-    displayList(chapter);
-  });
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function displayList(item) {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    
+    li.textContent = item;
+    deleteButton.textContent = '❌';
+    
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+    
+    deleteButton.addEventListener('click', function() {
+        list.removeChild(li);
+        deleteChapter(item);
+        input.focus();
+    });
+}
+
+function deleteChapter(chapter) {
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
