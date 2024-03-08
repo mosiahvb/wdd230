1900f135772481f6441ccc1988b8788b3786085d
Mosiah Valdez <val20004@byui.edu>
2024-03-07 17:29:24 -0700

    needs saved

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,8 +1,37 @@
-const url = "data/members.jason";
-async function getProphetData() {
+const url = "data/members.json";
+const card = document.querySelector(".directory");
+async function getData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.directory); // temporary testing of data retreival
+    displayDirectory(data.directory);
   }
   
-  getProphetData();
+  const displayDirectory = (directory) => {
+    directory.forEach(member => {
+        const sec = document.createElement('section');
+        const h2 = document.createElement('h2');
+        const phone_number = document.createElement('p');
+        const addresses = document.createElement('p');
+        const website = document.createElement('p');
+        const membership = document.createElement('p');
+        const img = document.createElement('img');
+
+        h2.textContent = `${member.names}`;
+        phone_number.textContent = `${member.phone_numbers}`;
+        addresses.textContent = `${member.addresses}`;
+        website.textContent = `${member.website_URLs}`;
+        membership.textContent = `${member.membership_level}`;
+        img.setAttribute('src', member.image);
+
+        sec.appendChild(h2);
+        sec.appendChild(phone_number);
+        sec.appendChild(addresses);
+        sec.appendChild(website);
+        sec.appendChild(membership);
+        sec.appendChild(img);
+
+        card.appendChild(sec); // Append sec to the cards div
+    });
+}
+
+  getData();
